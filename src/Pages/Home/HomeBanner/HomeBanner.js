f07f10e1bbda226ec6abcf38d5014197dae75aa5
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-11-21 22:25:31 +0600

    Done banner text

--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./HomeBanner.css";
 
 const HomeBanner = () => {
-  return <div>Home Banner</div>;
+  return (
+    <div className="homeBannerWrap">
+      <div className="bannerText">
+        <div>
+          <h3>Welcome to smiles kindergarten</h3>
+          <h1>the perfect place for</h1>
+          <h1> every child</h1>
+          <button>learn more</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeBanner;
