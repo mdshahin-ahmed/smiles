885bb142f604ff626b175637eb03f056fc27af1c
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-11-22 18:50:21 +0600

    Done home banner section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "../Shared/Header/Header";
 import HomeBanner from "./HomeBanner/HomeBanner";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="homeBannerWrap">
-      <Header></Header>
-      <HomeBanner></HomeBanner>
-    </div>
+    <>
+      <div className="HeaderBannerWrap">
+        <div className="headerBannerBg">
+          <Header></Header>
+          <HomeBanner></HomeBanner>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="white "
+              fill-opacity="1"
+              d="M0,224L48,229.3C96,235,192,245,288,224C384,203,480,149,576,128C672,107,768,117,864,149.3C960,181,1056,235,1152,240C1248,245,1344,203,1392,181.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -7,9 +7,9 @@ const HomeBanner = () => {
       <div className="bannerText">
         <div>
           <h3>Welcome to smiles kindergarten</h3>
-          <h1>the perfect place for</h1>
+          <h1 className="mt-5">the perfect place for</h1>
           <h1> every child</h1>
-          <button>learn more</button>
+          <button className="buttonStyle my-4 ">learn more</button>
         </div>
       </div>
     </div>
